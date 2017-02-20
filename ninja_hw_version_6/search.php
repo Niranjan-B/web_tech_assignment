4d68804c5d50613fcb394ec48884608fcb2ec52d
Niranjan-B <nero.niranjan@gmail.com>
2017-02-20 11:54:43 -0800

    renamed to search.php, added hide and visible functionalities for distance/location table row.

--- ninja_hw_version_6/search.php
+++ ninja_hw_version_6/search.php
@@ -21,21 +21,43 @@
             font-size:15px;
         }
     </style>
-    <body>
+
+    <script type="text/javascript">
+
+        function hideLocationDistanceRow() {
+            var locationDistanceRow = document.getElementById('location_distance_input_row');
+            // hide the row by default
+            locationDistanceRow.style.visibility = "hidden";
+        }
+
+        function onElementSelected() {
+            var selectTag = document.getElementById("type_of_search");
+            var locationDistanceRow = document.getElementById('location_distance_input_row');
+
+            if (selectTag.value === "places") {
+                locationDistanceRow.style.visibility = "visible";
+            } else {
+                locationDistanceRow.style.visibility = "hidden";
+            }
+        }        
+
+    </script>
+
+    <body onload="hideLocationDistanceRow()">
             <div class="facebook_search_container">
                 <center><span id="facebook_search_text">Facebook Search</span></center>
                 <hr style="color: grey; margin-left: 10px; margin-right:10px;">
-                <form method="post" action="" style="padding-left:10px;">
+                <form method="post" action="" style="padding-left:10px;" autocomplete="on">
                     <table>
                         <tr>
                             <td><label for="keyword" class="form_labels">Keyword:&nbsp;</label></td>
-                            <td><input type="text" id="keyword" name="keyword"/></td>
+                            <td><input type="text" id="keyword" name="keyword" required/></td>
                         </tr>
                         <tr>
                             <td><label for="type" class="form_labels">Type:&nbsp;</label></td>
                             <td>
-                                <select name="typeOfSearch">
-                                    <option value="users" selected>Users</option>
+                                <select name="typeOfSearch" id="type_of_search" onchange="onElementSelected()">
+                                    <option value="users" selected="selected">Users</option>
                                     <option value="pages">Pages</option>
                                     <option value="events">Events</option>
                                     <option value="places">Places</option>
@@ -43,7 +65,7 @@
                                 </select>
                             </td>
                         </tr>
-                        <tr>
+                        <tr id="location_distance_input_row">
                             <td><label for="location" class="form_labels">Location:&nbsp;</label></td>
                             <td><input type="text" id="location" name="location"/></td>
                             <td><label for="distance" class="form_labels">Distance(meters):&nbsp;</label></td>
