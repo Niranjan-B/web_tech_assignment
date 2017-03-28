fac9057da29f74af82018cb90e3764b79232bbae
Niranjan-B <nero.niranjan@gmail.com>
2017-03-28 13:52:48 -0700

    showing tool-tip when the form is invalid

--- ninja_hw_version_8/web-hw/src/app/app.component.ts
+++ ninja_hw_version_8/web-hw/src/app/app.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+declare var $:any;
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+    ngOnInit() : void {
+       $('[data-toggle="tooltip"]').tooltip({trigger: 'manual'});
+    }
+  
+    searchGroup = new FormGroup({
+      searchedKeyWord: new FormControl(null, Validators.required)
+    });
+    
+    // fire search if it's valid
+    fireSearchEvent(formValue: Object, formValidity: boolean): void {
+      if (!formValidity) {
+        var inputField = $('#searchQueryInput').tooltip('show');
+        setTimeout(function(){
+          inputField.tooltip('hide');
+        }, 1500);
+      } else {
+        // form valid here.. call the backend
+      }
+    }
+
 }
