a7156ac1b0334019f76d181823014e82d5a8820d
Niranjan-B <nero.niranjan@gmail.com>
2017-04-11 22:22:03 -0700

    about me activity completed

--- ninja_hw_version_9/WebTech/app/src/main/java/com/ninja/webtech/activities/AboutMeActivity.java
+++ ninja_hw_version_9/WebTech/app/src/main/java/com/ninja/webtech/activities/AboutMeActivity.java
@@ -2,14 +2,22 @@ package com.ninja.webtech.activities;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 import com.ninja.webtech.R;
 
 public class AboutMeActivity extends AppCompatActivity {
 
+    Toolbar mToolbar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_me);
+
+        mToolbar = (Toolbar) findViewById(R.id.toolbar_activity_about_me);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("About Me");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
