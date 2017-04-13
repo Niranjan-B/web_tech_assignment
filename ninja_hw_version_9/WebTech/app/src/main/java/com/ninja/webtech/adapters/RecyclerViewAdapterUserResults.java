71ce9d7c00a574bf61543caa23e1e344566766ae
Niranjan-B <nero.niranjan@gmail.com>
2017-04-13 12:05:02 -0700

    displaying list of users in recycler-view.

---
+++ ninja_hw_version_9/WebTech/app/src/main/java/com/ninja/webtech/adapters/RecyclerViewAdapterUserResults.java
@@ -0,0 +1,61 @@
+package com.ninja.webtech.adapters;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.ninja.webtech.R;
+import com.ninja.webtech.models.user.Datum;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
+/**
+ * Created by niranjanb on 13/04/17.
+ */
+
+public class RecyclerViewAdapterUserResults extends RecyclerView.Adapter<RecyclerViewAdapterUserResults.ViewHolder> {
+
+    ArrayList<Datum> mUsersList = new ArrayList<>();
+    Context mContext;
+
+    public RecyclerViewAdapterUserResults(ArrayList<Datum> usersList, Context context) {
+        this.mUsersList = usersList;
+        mContext = context;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.custom_row_recycler_view_results, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Picasso.with(mContext).load(mUsersList.get(position).getPicture().getData().getUrl()).into(holder.mProfilePic);
+        holder.mName.setText(mUsersList.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mUsersList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        ImageView mProfilePic;
+        TextView mName;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+
+            mProfilePic = (ImageView) itemView.findViewById(R.id.image_view_custom_row);
+            mName = (TextView) itemView.findViewById(R.id.text_view_custom_row);
+        }
+    }
+
+}
--- ninja_hw_version_9/WebTech/app/src/main/java/com/ninja/webtech/fragments/UsersResultsFragment.java
+++ ninja_hw_version_9/WebTech/app/src/main/java/com/ninja/webtech/fragments/UsersResultsFragment.java
@@ -3,25 +3,34 @@ package com.ninja.webtech.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.ninja.webtech.R;
+import com.ninja.webtech.adapters.RecyclerViewAdapterUserResults;
 import com.ninja.webtech.models.user.Users;
 import com.ninja.webtech.network.RetrofitManager;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
 import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.schedulers.Schedulers;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class UsersResultsFragment extends Fragment {
+
+    private RecyclerView mRecyclerView;
+    private RecyclerViewAdapterUserResults mRecyclerViewAdapter;
+    private CompositeDisposable mCompositeDisposable;
 
     public static UsersResultsFragment getNewInstance() {
         return new UsersResultsFragment();
@@ -36,6 +45,13 @@ public class UsersResultsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_users_results, container, false);
+        mRecyclerView = (RecyclerView) view.findViewById(R.id.recycler_view_user_results);
+        mCompositeDisposable = new CompositeDisposable();
+
+        initRecyclerView();
+
         Bundle bundle = getArguments();
         if (bundle != null) {
             Log.d("users", getArguments().getString("query"));
@@ -45,15 +61,22 @@ public class UsersResultsFragment extends Fragment {
             queryMap.put("search_type", "users");
             queryMap.put("searched_keyword", getArguments().getString("query"));
 
-            RetrofitManager.getRetrofitInstance().getQueriedUsers(queryMap)
+            mCompositeDisposable.add(
+                    RetrofitManager.getRetrofitInstance().getQueriedUsers(queryMap)
                     .subscribeOn(Schedulers.io())
                     .observeOn(AndroidSchedulers.mainThread())
-                    .subscribe(this::handleResponse, this::handleError);
+                    .subscribe(this::handleResponse, this::handleError)
+            );
 
         } else {
             Log.d("users", "null bundle");
         }
-        return inflater.inflate(R.layout.fragment_users_results, container, false);
+        return view;
+    }
+
+    private void initRecyclerView() {
+        mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
     }
 
     private void handleError(Throwable throwable) {
@@ -61,7 +84,13 @@ public class UsersResultsFragment extends Fragment {
     }
 
     private void handleResponse(Users users) {
-        Log.d("ninja", ""+users.getData());
+        mRecyclerViewAdapter = new RecyclerViewAdapterUserResults(new ArrayList<>(users.getData()), getContext());
+        mRecyclerView.setAdapter(mRecyclerViewAdapter);
     }
 
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        mCompositeDisposable.clear();
+    }
 }
